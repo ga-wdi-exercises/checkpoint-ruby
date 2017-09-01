2cd0169f107e33003abd4d59816174155266ed71
Spenser Holstein <spenser.holstein@gmail.com>
2017-09-01 09:43:27 -0400

    Sorry, only 1 commit. All Completed.

--- fundamentals.rb
+++ fundamentals.rb
@@ -4,10 +4,9 @@
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
 
-
-
-
-
+def offer_rose (person)
+  puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 # Question 2
 # Assume the following hash...
@@ -26,11 +25,8 @@ town = {
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
 
-
-
-
-
-
+town[:residents].delete("Belle")
+town[:castle][:guests].push("Belle")
 
 # Question 3
 # Assume you have an array of strings representing friends' names...
@@ -43,3 +39,7 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+for friend in friends do
+  puts "Belle is friends with #{friend}"
+end
