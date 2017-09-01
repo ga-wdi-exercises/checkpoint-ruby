a49928076b8099f034adfa2f11e5a05de4217181
Drew Dandrea <d.dandrea00@gmail.com>
2017-09-01 09:44:16 -0400

    fundamentals and enums done

--- fundamentals.rb
+++ fundamentals.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Question 1
 # Define a method called `offer_rose`, which should take one argument named `person` (String).
 # When called the method should print to the terminal:
@@ -5,8 +7,9 @@
 # Type your solution directly below this line:
 
 
-
-
+def offer_rose(person)
+  puts "Would you take this rose. #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 
 # Question 2
@@ -26,11 +29,8 @@ town = {
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
 
-
-
-
-
-
+ town[:residents].delete("Belle")
+ town[:castle][:guests].push("Belle")
 
 # Question 3
 # Assume you have an array of strings representing friends' names...
@@ -43,3 +43,11 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+  for friend in friends
+    puts friend
+  end
+
+  for friend in friends
+    puts "Belle is friends with #{friend}"
+  end
