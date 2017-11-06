884de3742ea9be6ca50cce0b82ee3e097fd9e979
Sloan Holzman <sloan.holzman@gmail.com>
2017-11-06 09:04:26 -0500

    complete fundamentals.rb...haven't tested yet

--- fundamentals.rb
+++ fundamentals.rb
@@ -3,8 +3,9 @@
 # When called the method should print to the terminal:
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
-
-
+def offer_rose (person)
+  puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 
 
@@ -25,8 +26,8 @@ town = {
 # - Remove "Belle" from `residents`
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
-
-
+town[:residents].delete(1);
+town[:guests].push("Belle");
 
 
 
@@ -43,3 +44,6 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+for friend in friends
+  puts "Belle is friends with #{friend}"
+end
