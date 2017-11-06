f90f5ace05e00330885b5d9f9157223acad7f59b
Farris <farris.ismati@gmail.com>
2017-11-06 09:31:21 -0500

    OOP done

--- oop.rb
+++ oop.rb
@@ -5,15 +5,23 @@
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
 
+class Animal
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
 
+  def greet
+    puts "Hi, I'm #{@name}"
+  end
+end
 
 # Question 7
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
 
-
-
+pumba = Animal.new("Pumba")
 
 # Question 8
 # Define a Ruby class called `Lion` that inherits from the `Animal` class.
@@ -23,10 +31,25 @@
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
 
+class Lion < Animal
+  attr_reader :king
 
+  def initialize(name)
+    @name = name
+    @king = check_king
+  end
 
-
+  def check_king
+    if @name == "Simba"
+      @king = true
+    else
+      @king = false
+    end
+  end
+end
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+
+simba = Lion.new("Simba")
