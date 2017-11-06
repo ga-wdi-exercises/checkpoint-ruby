70ecf3753f74fdfb0381faf7853873c2937a8105
Michael Ortiz <ortiz_michael@bah.com>
2017-11-06 09:42:11 -0500

    initial attempt

--- oop.rb
+++ oop.rb
@@ -1,3 +1,4 @@
+
 # Question 6
 # Define a Ruby class called `Animal`. Each `Animal` should have...
 # - A `name` (String) attribute
@@ -5,13 +6,25 @@
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
 
+class Animal
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Howdy! My name is #{@name}"
+  end
+end
 
 
 # Question 7
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
 
+jerry = Animal.new('Jerry')
+jerry.greet
 
 
 
@@ -23,10 +36,27 @@
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
 
+class Lion < Animal
 
+  def initialize(name)
+    super(name)
+    @king = false
+  end
 
+  def is_king
+    if name == 'Simba'
+      @king = true
+      return @king
+    else
+      @king = false
+      return @king
+    end
+  end
+end
 
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+
+simba = Lion.new('Simba')
