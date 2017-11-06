bf3c99fed6fe178a49c4e0b86f7c03e771ecba1a
Casey Jordan <Jordan_Casey.bah.com>
2017-11-06 09:36:19 -0500

    oop done

--- oop.rb
+++ oop.rb
@@ -1,9 +1,23 @@
+require("pry")
 # Question 6
 # Define a Ruby class called `Animal`. Each `Animal` should have...
 # - A `name` (String) attribute
 # - A `greet` instance method
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
+
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hi my name is #{@name}"
+  end
+end
+
 
 
 
@@ -12,7 +26,7 @@
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
 
-
+Pumba = Animal.new("Pumba")
 
 
 # Question 8
@@ -23,10 +37,25 @@
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
 
+class Lion < Animal
+  attr_accessor :king
 
+  def initialize(name)
+    super(name)
+    if name == "Simba"
+      @king = true
+    elsif name != "Simba"
+      @king = false
+    end
+  end
+end
 
 
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+
+Simba = Lion.new("Simba")
+
+binding.pry
