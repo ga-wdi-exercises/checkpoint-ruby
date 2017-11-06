7231a413c0653a0510edbed4ca43aad322021421
Casey Jordan <Jordan_Casey.bah.com>
2017-11-06 09:36:02 -0500

    everything but adding belle works

--- fundamentals.rb
+++ fundamentals.rb
@@ -1,10 +1,13 @@
+require("pry")
 # Question 1
 # Define a method called `offer_rose`, which should take one argument named `person` (String).
 # When called the method should print to the terminal:
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
 
-
+def offer_rose(person)
+  puts "would you take this rose " + person + "in exchange for giving and old beggar woman shelter from the bitter cold?"
+end
 
 
 
@@ -26,6 +29,8 @@ town = {
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
 
+town[:residents].delete("Belle")
+
 
 
 
@@ -43,3 +48,10 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+for i in 0..friends.length-1 do
+  puts "Belle is friends with #{friends[i]}"
+end
+
+
+binding.pry
