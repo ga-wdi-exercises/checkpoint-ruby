8e20758c9a2268166bcf4631e5c0a83474e39506
Dan McQuade <dtmcquade@gmail.com>
2017-11-06 09:34:28 -0500

    Complete fundamentals section

--- fundamentals.rb
+++ fundamentals.rb
@@ -5,6 +5,9 @@
 # Type your solution directly below this line:
 
 
+def offer_rose(person)
+  puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 
 
@@ -27,6 +30,9 @@ town = {
 # Type your solution directly below this line:
 
 
+town[:residents].delete("Belle")
+
+town[:castle][:guests].push("Belle")
 
 
 
@@ -43,3 +49,7 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+for friend in friends
+  puts "Belle is friends with #{friend}"
+end
