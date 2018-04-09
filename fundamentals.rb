0dc4e3b7575d6dc843ba0e50cee1e00ea78cdf06
Daniel Freudberg <dbfreudber@gmail.com>
2018-04-09 09:16:13 -0400

    rspec not working, but I think I did this right. initial commit

--- fundamentals.rb
+++ fundamentals.rb
@@ -5,8 +5,9 @@
 # Type your solution directly below this line:
 
 
-
-
+def offer_rose (person)
+  puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 
 # Question 2
@@ -26,7 +27,8 @@ town = {
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
 
-
+town[:residents].delete_at(1)
+town[:castle][:guests].push("Belle")
 
 
 
@@ -43,3 +45,7 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+friends.length.times do |i|
+  puts "Belle is friends with #{friends[i]}"
+end 
