20c2790610d0e4969a207505ad8a938418f8e7c8
Sam Gehret <sam.gehret@gmail.com>
2018-04-09 09:33:07 -0400

    completed fundamentals

--- enums.rb
+++ enums.rb
@@ -13,8 +13,9 @@ lost_boys = [
 # 30 years.
 # Type your solution directly below this line:
 
-
-
+# lost_boys.each do |boy|
+#   boy[:age]+= 30
+# end
 
 
 
@@ -27,3 +28,5 @@ children = ['Wendy', 'John', 'Michael']
 # of their names. Assign the returned array to a variable called `darling_children`.
 # Example: `Wendy` should become `Wendy Darling` in the new array.
 # Type your solution directly below this line:
+
+# darling_children = children.map {|child| child + " Darling"}
