5e9b36502ce413152247ff4ce2fc2fe825ad162a
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-04-09 09:50:49 -0400

    how do I inheritance Lolgit add -Agit add -A?

--- oop.rb
+++ oop.rb
@@ -5,6 +5,17 @@
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
 
+class Animal
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+
+    def greet(name)
+        puts name
+    end
+end
+
 
 
 
@@ -12,6 +23,7 @@
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
 
+pumba = Animal.new("Pumba")
 
 
 
@@ -23,10 +35,14 @@
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
 
-
+class Lion < Animal
+# lol
+end
 
 
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+
+simba = Lion.new("Simba")
